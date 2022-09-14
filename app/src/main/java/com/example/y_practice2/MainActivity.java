be0573ed8bbd1a1904a4a802dd187973ce969817
IWannaGoMars <73753121+yeongmin7870@users.noreply.github.com>
2022-09-14 11:49:19 +0900

    박영민 Test

--- app/src/main/java/com/example/y_practice2/MainActivity.java
+++ app/src/main/java/com/example/y_practice2/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        다메다메
+//        다메다메d
     }
 }

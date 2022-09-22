b6a477bfd651a425bd4a0b29edea0697acb53793
IWannaGoMars <73753121+yeongmin7870@users.noreply.github.com>
2022-09-22 18:21:46 +0900

    busking category 완료

--- app/src/main/java/com/example/y_practice2/busking_category.java
+++ app/src/main/java/com/example/y_practice2/busking_category.java
@@ -1,23 +1,23 @@
 package com.example.y_practice2;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ListView;
 
-public class detailed_category_1 extends AppCompatActivity {
+public class busking_category extends AppCompatActivity {
 
-    String [] fruitsList = {"오늘은 새우깡","남쪽으로 도망가","쏘아올린 북극곰",
+    String [] buskingList = {"오늘은 새우깡","남쪽으로 도망가","쏘아올린 북극곰",
             "오늘은 새우깡","남쪽으로 도망가","쏘아올린 북극곰"};
     ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detailed_category1);
+        setContentView(R.layout.activity_busking_category);
 
-        listView = findViewById(R.id.v_list);
-        CustomBaseAdapter customBaseAdapter = new CustomBaseAdapter(getApplication(),fruitsList);
+        listView = findViewById(R.id.busking_list);
+        CustomBaseAdapter customBaseAdapter = new CustomBaseAdapter(getApplication(),buskingList);
         listView.setAdapter(customBaseAdapter);
 
     }

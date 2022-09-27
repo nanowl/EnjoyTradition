44dedea563f5c2fcfde314de1b642799b1bb5f57
nanowl <hanjy20129@gmail.com>
2022-09-27 19:47:08 +0900

    1:1문의 페이지 추가 색깔 수정 필요?

--- app/src/main/java/com/example/y_practice2/MainActivity.java
+++ app/src/main/java/com/example/y_practice2/MainActivity.java
@@ -1,65 +1,47 @@
 package com.example.y_practice2;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.question_and_answer);
 //        getHashKey(); //키해시를 호출
 
-        ImageButton button = findViewById(R.id.movie);
-        BottomNavigationView menu = findViewById(R.id.bottomNavigationView);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),Categorie_detail.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                startActivity(intent);
-            }
-        });
-        menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.person:
-//                        Intent mypage = new Intent(getApplicationContext(),)
-                        return true;
-                    case R.id.search:
-                        Intent search = new Intent(getApplicationContext(),Map_UserInterface.class);
-                        startActivity(search);
-                        overridePendingTransition(0, 0); //애니메이션 없애기
-                    case R.id.home:
-                        Intent homepage = new Intent(getApplicationContext(),MainActivity.class);
-                        startActivity(homepage);
-                        overridePendingTransition(0, 0); //애니메이션 없애기
-                }
-                return false;
-            }
-        });
+//        ImageButton button = findViewById(R.id.movie);
+//        BottomNavigationView menu = findViewById(R.id.bottomNavigationView);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(),Categorie_detail.class);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+//                startActivity(intent);
+//            }
+//        });
+//        menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId()){
+//                    case R.id.person:
+////                        Intent mypage = new Intent(getApplicationContext(),)
+//                        return true;
+//                    case R.id.search:
+//                        Intent search = new Intent(getApplicationContext(),Map_UserInterface.class);
+//                        startActivity(search);
+//                        overridePendingTransition(0, 0); //애니메이션 없애기
+//                    case R.id.home:
+//                        Intent homepage = new Intent(getApplicationContext(),MainActivity.class);
+//                        startActivity(homepage);
+//                        overridePendingTransition(0, 0); //애니메이션 없애기
+//                }
+//                return false;
+//            }
+//        });
     }
 
 

0448fa457789a33925644f059e1602e9d083800a
tjrwns <tjrwns1202@naver.com>
2022-09-28 10:04:32 +0900

    로그인,회원가입 페이지

---
+++ app/src/main/java/com/example/y_practice2/Login_main.java
@@ -0,0 +1,14 @@
+package com.example.y_practice2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main);
+    }
+}
---
+++ app/src/main/java/com/example/y_practice2/Sign_up.java
@@ -0,0 +1,4 @@
+package com.example.y_practice2;
+
+public class Sign_up {
+}

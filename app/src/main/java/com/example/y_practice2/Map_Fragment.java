aded6fcf933e41556c41c4e55ded7273e8688611
tjrwns <tjrwns1202@naver.com>
2022-09-28 12:44:10 +0900

    하단탭연결

--- app/src/main/java/com/example/y_practice2/Map_Fragment.java
+++ app/src/main/java/com/example/y_practice2/Map_Fragment.java
@@ -6,20 +6,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 import net.daum.mf.map.api.MapView;
 
-public class Map_Fragment extends Fragment  {
+public class Map_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
         View view = inflater.inflate(R.layout.map_interface_frame, container, false);
-        MapView map = view.findViewById(R.id.map_view);
+        MapView map = new MapView(getActivity());
+
 
         ViewGroup mapViewContainer = (ViewGroup) view.findViewById(R.id.map_view);
         mapViewContainer.addView(map);
 
         return view;
     }
+
 
 }

5b38d9b1fa9f89b4077f09eb740d647611d9be1e
IWannaGoMars <73753121+yeongmin7870@users.noreply.github.com>
2022-09-28 10:18:03 +0900

    회원정보 수정 페이지

---
+++ app/src/main/java/com/example/y_practice2/Edit_member.java
@@ -0,0 +1,14 @@
+package com.example.y_practice2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Edit_member extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_member);
+    }
+}

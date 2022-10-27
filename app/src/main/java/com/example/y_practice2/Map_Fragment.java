557099af41249e2d6104822e126fca15fd587aaf
tjrwns <tjrwns1202@naver.com>
2022-10-27 14:00:12 +0900

    지도 나오는지 테스트

--- app/src/main/java/com/example/y_practice2/Map_Fragment.java
+++ app/src/main/java/com/example/y_practice2/Map_Fragment.java
@@ -1,6 +1,7 @@
 package com.example.y_practice2;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,11 +11,16 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 public class Map_Fragment extends Fragment {
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
         View view = inflater.inflate(R.layout.map_interface_frame, container, false);
@@ -23,6 +29,13 @@ public class Map_Fragment extends Fragment {
 
         ViewGroup mapViewContainer = (ViewGroup) view.findViewById(R.id.map_view);
         mapViewContainer.addView(map);
+        Log.d("test",map.toString());
+
+        // 중심점 변경 - 예제 좌표는 서울 남산
+        map.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304), true);
+
+        // 줌 레벨 변경
+        map.setZoomLevel(4, true);
 
         return view;
     }
